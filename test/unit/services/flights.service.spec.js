598303ac941271c238fe3cf4131b390593b75e1d
Rafael Reis <rafaelfc.ads@gmail.com>
2019-03-02 15:01:42 -0300

    add karma config

--- test/unit/services/flights.service.spec.js
+++ test/unit/services/flights.service.spec.js
@@ -3,14 +3,14 @@ describe('/app/services/flights', () => {
 
     let FlightService
 
-    // beforeEach(angular.mock.module('app.flight'))
+    beforeEach(angular.mock.module('app.flight'))
 
     beforeEach(inject(Flights => {
-        // FlightService = Flights
+        FlightService = Flights
     }))
 
     it('can be instantiated FlightService', () => {
-        // expect(FlightService).not.toBeDefined()
+        expect(FlightService).not.toBeDefined()
     })
 
  
