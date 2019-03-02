a105cac0a7cb4f019fa309079e42cf48d40085cc
Rafael Reis <rafaelfc.ads@gmail.com>
2019-03-02 14:44:45 -0300

    add flights.service tests

---
+++ test/unit/services/flights.service.spec.js
@@ -0,0 +1,17 @@
+'use strict'
+describe('/app/services/flights', () => {
+
+    let FlightService
+
+    // beforeEach(angular.mock.module('app.flight'))
+
+    beforeEach(inject(Flights => {
+        // FlightService = Flights
+    }))
+
+    it('can be instantiated FlightService', () => {
+        // expect(FlightService).not.toBeDefined()
+    })
+
+ 
+})
